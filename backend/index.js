908ad559868d73755423239c600ec10871b196d8
James McConnell <interneth3ro@gmail.com>
2024-02-12 18:20:04 -0600

    added socket io to backend

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,27 @@
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import cors from "cors";
-import dotenv from 'dotenv';
-import userRouter from "./src/api/user.js";
-
-dotenv.config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { Server } from 'socket.io';
 
 const app = express();
-app.use(bodyParser.json({ limit: "5mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+const PORT = process.env.PORT || 8080;
+app.use(bodyParser.json({ limit: '5mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 
 app.use(cors());
-app.use("/api/user", userRouter);
 
-const PORT = process.env.PORT || 5000;
+let server = app.listen(PORT, () =>
+  console.log(`Server Started On Port ${PORT}`)
+);
 
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server Started On Port ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
+let io = new Server(server);
+io.on('connection', (socket) => {
+  console.log(`socket.io connected: ${socket.id}`);
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  socket.on('disconnect', () => {
+    console.log(`Client ${socket.id} disconnected`);
+    socket.leave(roomId);
+  });
+});
